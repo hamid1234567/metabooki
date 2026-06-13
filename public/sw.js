7ce2d8871806181af35f1f4ae48cee2af4648c9c
Mohammadi <mohammadi219@gmail.com>
2026-06-13 19:09:52 +0330

    Auto sync: 2026-06-13 19:09

---
+++ public/sw.js
@@ -0,0 +1,52 @@
+const APP_VERSION = '1.0.4'
+const CACHE_NAME = `metabooki-${APP_VERSION}`
+
+const PRECACHE_URLS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './favicon.png',
+  './icon-192.png',
+  './icon-512.png',
+  './metabooki-logo.png',
+  './version.json',
+]
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting()
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS)))
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => Promise.all(cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then((clients) => Promise.all(clients.map((client) => {
+        const url = new URL(client.url)
+        if (url.searchParams.get('appVersion') === APP_VERSION) return
+        url.searchParams.set('appVersion', APP_VERSION)
+        return client.navigate(url.toString())
+      })))
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+
+  const url = new URL(event.request.url)
+  if (url.origin !== self.location.origin) return
+
+  event.respondWith(
+    fetch(event.request, { cache: 'no-store' })
+      .then((response) => {
+        if (response.ok) {
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
+        }
+        return response
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+  )
+})
