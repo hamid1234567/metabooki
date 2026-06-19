802f4b61ad57157b54f0c8606d67f8c280062fa4
Mohammadi <mohammadi219@gmail.com>
2026-06-19 13:10:43 +0330

    Auto sync: 2026-06-19 13:10

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '1.0.146'
+const APP_VERSION = '1.0.147'
 const CACHE_NAME = `metabooki-${APP_VERSION}`
 
 const PRECACHE_URLS = [
--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = '1.0.146'
+export const APP_VERSION = '1.0.147'
 export const APP_LABEL = 'Metabooki'
