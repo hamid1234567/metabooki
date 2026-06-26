1465e860b3d2d092b798fe2e8ef31b84d6e3e503
Mohammadi <mohammadi219@gmail.com>
2026-06-26 16:15:46 +0330

    Enable GitHub Pages for private repo

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '1.0.455'
+const APP_VERSION = '1.0.456'
 const CACHE_NAME = `metabooki-${APP_VERSION}`
 
 const PRECACHE_URLS = [
--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = '1.0.455'
+export const APP_VERSION = '1.0.456'
 export const APP_LABEL = 'Metabooki'
